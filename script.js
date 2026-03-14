570c1aa8416c9786a394ea8c4b18075a27a29555
sandyruswandi71-prog <sandyruswandi71@gmail.com>
2026-03-14 07:26:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function check(){
+
+let password = "numa";
+
+let input = document.getElementById("pass").value;
+
+if(input === password){
+
+document.querySelector(".login").style.display = "none";
+document.getElementById("main").style.display = "block";
+
+}else{
+
+alert("Wrong password ❤️");
+
+}
+
+}
+
+function win(){
+
+alert("I love you Alisya ❤️ - Sandy");
+
+}
